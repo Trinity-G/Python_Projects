7fce1402109ff5faf9139785e583f02c8e9ee82f
Trinity-G <Lorenzogallegos498@gmail.com>
2022-06-28 17:33:30 -0600

    main.py

---
+++ Space_Invaded/main.py
@@ -0,0 +1,46 @@
+import pygame, os, time, random
+pygame.font.init()
+
+# Game Window
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Title
+pygame.display.set_caption('Invaded_Space')
+
+# Load Spaceship images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+# Player Ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_yellow.png'))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_red.png'))
+GREEN_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_green.png'))
+BLUE_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_blue.png'))
+YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'))
+
+# Background
+BG = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'background-black.png')), (WIDTH, HEIGTH))
+
+# Laser class
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+    
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0 )
+    
+    def collision(self, obj):
+        return collide(self, obj)
