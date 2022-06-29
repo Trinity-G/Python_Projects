21c633fd9d0dc974fce4b733ed1654d87552e924
Trinity-G <Lorenzogallegos498@gmail.com>
2022-06-29 13:01:41 -0600

    main.py

--- Space_Invaded/main.py
+++ Space_Invaded/main.py
@@ -8,7 +8,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 # Title
 pygame.display.set_caption('Invaded_Space')
 
-# Load Spaceship images
+# Invading Spaceship images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
@@ -44,3 +44,54 @@ class Laser:
     
     def collision(self, obj):
         return collide(self, obj)
+
+# Ship class
+class ship:
+    COOLDOWN = 30
+
+    def __init__(self, x, y, health = 100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_image = None
+        self.laser_image = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_image, (self.x, self.y ))
+        for lasers in self.lasers:
+            laser.draw(window)
+
+    def move_lasers(self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+            self.lasers.remove(laser)
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_image)
+            self.laser.append(laser)
+            self.cool_down_counter = 1
+
+    def get_width(self):
+        return self.ship_image.get_width()
+
+    def get_height(self):
+        return self.ship_image.get_height()
+
+# player class
+class Player(ship):
+    def __init__(self, x, y, health = 100):
+        super.__init__(x, y, health)
