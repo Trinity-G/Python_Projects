193f240da5c0931eb6a5b8a886c38c211df6b754
LorenzoG <Lorenzogallegos498@gmail.com>
2022-12-19 20:10:47 -0600

    Assets/Images/blue_carbon_background.jpg

--- Visual_Novel/main.py
+++ Visual_Novel/main.py
@@ -11,10 +11,10 @@ RED = (255, 0, 0)
 TEAL = (0, 80, 80)
 
 # Display Background
-size = (700, 500)
+size = (800, 768)
 screen = pygame.display.set_mode(size)
-bg_img = pygame.image.load()
-bg_img = pygame.transform.scale(bg_image, (size))
+bg_img = pygame.image.load('Assets/Images/blue_carbon_background.jpg')
+bg_img = pygame.transform.scale(bg_img, (size))
 pygame.display.set_caption("Furry Playhouse")
 
 
@@ -26,21 +26,22 @@ clock = pygame.time.Clock()
 
 # Program loop
 while mainGameloop:
-    
+    screen.blit(bg_img,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             mainGameloop = False
+    pygame.display.update()
+pygame.quit()
 
 # Initialize mixer in the program
 mixer.init()
-mixer.music.load('Music File/bensound-summer_wav_music.wav')
+mixer.music.load('Assets/Music_Files/')
 mixer.music.play()
 
 # Clears the screen
-    screen.fill(TEAL)
+screen.fill(TEAL)
 
 # Updates the screen with what's been drawn
-    pygame.display.flip()
+pygame.display.flip()
 # FPS
-    clock.tick(60)
-
+clock.tick(60)
